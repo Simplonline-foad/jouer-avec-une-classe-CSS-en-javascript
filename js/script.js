d8d4a14d6e02a5f9f35d28615180360f96e9d1b6
Simplonline <julie-garnier@live.fr>
2015-11-09 21:06:34 +0000

    ajout code

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$liste = document.getElementById("liste");
+
+function selectionne(e) {
+  // e.target correspond au <li> qui a été cliqué dans le <ul> !
+  e.target.classList.toggle("selected");
+}
+
+$liste.onclick = selectionne;
