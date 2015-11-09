62f48797c97e6da4f9c57460e0b654cc872f38e2
Simplonline <julie-garnier@live.fr>
2015-11-09 21:27:53 +0000

    ajout code final

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 var $liste = document.getElementById("liste");
+var $supprimer = document.getElementById("supprimer");
+var $restaurer = document.getElementById("restaurer");
 
-function selectionne(e) {
-  // e.target correspond au <li> qui a été cliqué dans le <ul> !
-  e.target.classList.toggle("selected");
+function select(e) {
+    e.target.classList.toggle("selected");
 }
 
-$liste.onclick = selectionne;
+function supprimer() {
+    var i;
+    for (i = 0; i < $liste.children.length; i++) {
+        if ($liste.children[i].classList.contains("selected")) {
+            $liste.children[i].classList.add("poubelle");
+        }
+    }
+}
+
+function restaurer() {
+    var i;
+    for (i = 0; i < $liste.children.length; i++) {
+        $liste.children[i].classList.remove("poubelle");
+    }
+}
+
+$liste.onclick = select;
+$supprimer.onclick = supprimer;
+$restaurer.onclick = restaurer;
